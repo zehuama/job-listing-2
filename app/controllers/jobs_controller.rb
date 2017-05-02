c37401538ca0ceed0a7746ecf944d13ba94c3d39
zehuama <zehua046@gmail.com>
2017-05-02 19:37:36 +0800

    implement wage part

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,6 +1,6 @@
 class JobsController < ApplicationController
  before_action :authenticate_user!, only: [:new, :create, :update, :edit, :destroy]
- 
+
   def index
     @jobs = Job.all
   end
@@ -47,5 +47,5 @@ end
 private
 
 def job_params
-  params.require(:job).permit(:title, :description)
+  params.require(:job).permit(:title, :description, :wage_upper_bound, :wage_lower_bound, :contact_email)
 end
